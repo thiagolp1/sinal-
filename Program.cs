e4ce302b000b0500ed8d61ae8fb6faf19e5e12ff
Thiago <thiagolopespereiralp1@gmail.com>
2022-04-01 22:26:48 -0300

    finalizacão do exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numerod;
+
+Console.Write("Digite um número: ");
+numerod = Convert.ToInt32(Console.ReadLine());
+
+if (numerod <0)
+{    
+    Console.WriteLine("Negativo");
+}    
+else if (numerod >0)
+{
+    Console.WriteLine("Positvo");
+}    
+else 
+{
+    Console.WriteLine("Zero");
+}        
